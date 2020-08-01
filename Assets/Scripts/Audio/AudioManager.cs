c90ccb03a3ad0602ea922cc609d0e3320a06f987
Angelord <Georgiev1997@gmail.com>
2020-08-01 14:50:16 +0300

    Add sound effects to main menu buttons.

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Audio {
+    public class AudioManager : MonoBehaviour {
+
+        public AK.Wwise.Event BtnHoverEvent;
+    
+        public AK.Wwise.Event BtnClickEvent;
+
+        private static AudioManager instance;
+
+        public static AudioManager Instance => instance;
+
+        private void Start() {
+            if (instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+            
+            DontDestroyOnLoad(this);
+        }
+
+        public void OnButtonHover() {
+            BtnHoverEvent.Post(gameObject);
+        }
+
+        public void OnButtonPress() {
+            BtnClickEvent.Post(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/ButtonSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Audio {
+    [RequireComponent(typeof(Button))]
+    public class ButtonSound : MonoBehaviour {
+
+        public void OnMouseOver() {
+            AudioManager.Instance.OnButtonHover();
+        }
+
+        public void OnMouseDown() {
+            AudioManager.Instance.OnButtonPress();
+        }
+    }
+}
